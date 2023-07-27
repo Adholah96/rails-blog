42c827c89431334ed4ccc1ab4c16ea1fe25476a7
Adholah96 <adholah96@gmail.com>
2023-07-27 18:12:34 +0300

    Add tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Creating User with ID 3
+@user1 = User.create(id: 3, name: 'Tom', photo: 'https://unsplash.com/photos', bio: 'Teacher from Mexico.', posts_counter: 0)
+
+# Creating Post associated with User with ID 3
+@first_post = Post.create(author: @user1, title: 'post1', text: 'This is my first post', likes_counter: 0, comments_counter: 0)
+
+# Creating Comment associated with User with ID 3 and Post with ID 10
+@comment1 = Comment.create(post: @first_post, author: @user1, text: 'Hi Tom!, Nice comment')
